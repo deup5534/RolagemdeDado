3305ab44deef33d7f0d08805d19fec7befe29162
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-17 18:30:06 -0300

    o rpg pode ser feito agora

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Dados disponíveis");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nDado de 4 lados");
+Console.WriteLine("Dado de 6 lados");
+Console.WriteLine("Dados de 8 lados");
+Console.WriteLine("Dado de 10 lados");
+Console.WriteLine("Dado de 12 lados");
+Console.WriteLine("Dado de 20 lados");
+Console.WriteLine("Dado de 100 lados");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.Clear();
+
+
+Console.Write("Qual dado você escolhe? ");
+int dado = Convert.ToInt32(Console.ReadLine());
+
+int rolada = RandomNumberGenerator.GetInt32(1, dado + 1);
+if (rolada == 1)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"\nVocê tirou {rolada}... DESASTRE!");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine($"\nRolagem: {rolada}");
+    Console.ResetColor();
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # RolagemdeDado
 aquele RPG do bom ツ
+
+Um dado comum possui seis faces numeradas de 1 a 6. Existem dados poliédricos com as mais diversas contagens de faces, sendo as mais comuns 4, 6, 8, 10, 12, e 20.
+
+Faça um programa que receba a quantidade de faces do dado e sorteie aleatoriamente um valor entre 1 e a quantidade de faces informada, simulando uma rolagem.
+
+Exemplos:
+
+--- Rolagem de Dado ---
+
+Quantas faces? 12
+
+Rolagem: 11
+--- Rolagem de Dado ---
+
+Quantas faces? 12
+
+Rolagem: 3
